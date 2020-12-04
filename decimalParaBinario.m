d3f4b9da09ed67d947f9065ad8e0f09186828a22
Magno <magnocostamaia@gmail.com>
2020-12-04 18:24:49 -0300

    att

--- decimalParaBinario.m
+++ decimalParaBinario.m
@@ -1,7 +1,7 @@
 function bit = decimalParaBinario(x)
-    bit = zeros(1,512);
-    k = 2;
-    for j = 0:32:480
+    bit = zeros(1,15680);
+    k = 1;
+    for j = 0:32:15648
         n = 1 + j;
         for i = 31:-1:0
             if ((x(k) - (2^i)) >= 0)
